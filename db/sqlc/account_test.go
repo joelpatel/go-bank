7eb8393c7265bc49826adb7383b167da82db6ceb
joelpatel <joelpatel.cs@gmail.com>
2022-11-08 00:42:15 -0500

    add: test for GetAccount(...)

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -5,12 +5,13 @@ import (
 	"context"
 	"strconv"
 	"testing"
+	"time"
 
 	"github.com/joelpatel/go-bank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	args := CreateAccontParams{
 		Owner:    util.RandomOwner(),
 		Balance:  strconv.FormatInt(util.RandomMoney(), 10),
@@ -27,4 +28,23 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
