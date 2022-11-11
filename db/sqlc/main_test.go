93e2a0a1ee78fd7c5d22df96ad1e8385238b9654
joelpatel <joelpatel.cs@gmail.com>
2022-11-10 20:20:27 -0500

    modify: globally available test database connection object

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,16 +15,20 @@ const (
 	dbSource = "postgresql://root:password@localhost:5432/bank?sslmode=disable"
 )
 
-var testQueries *Queries
+var (
+	testQueries *Queries
+	testDB      *sql.DB
+)
 
 // Main entry point of all unit tests inside ONE specific golang PACKAGE.
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatalf("cannot connect to the database: %v\n", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
