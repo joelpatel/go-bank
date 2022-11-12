8ea39ed897b791f70f769829cbef225394f1af31
joelpatel <joelpatel.cs@gmail.com>
2022-11-12 17:51:15 -0500

    add: deadlock test

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -126,3 +126,58 @@ func TestTransferTx(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, account2.Balance+float64(n)*amount, updatedAccount2.Balance)
 }
+
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	// fmt.Printf(">> before:\t%v\t%v\n", account1.Balance, account2.Balance)
+
+	n := 10 // 5-> account 1 to 2 and 5-> account 2 to 1
+	amount := float64(10.1)
+
+	errors := make(chan error)
+
+	// run n go routines to exhaustively test database transactions are 100% working correctly
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errors <- err
+		}()
+	}
+
+	// check result and err
+	for i := 0; i < n; i++ {
+		err := <-errors
+		require.NoError(t, err)
+	}
+
+	// check final updated balances
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	// fmt.Printf(">> after:\t%v\t%v\n", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	// account1.Balance == updatedAccount1.Balance
+	require.NoError(t, err)
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	// account2.Balance == updatedAccount2.Balance
+	require.NoError(t, err)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+}
