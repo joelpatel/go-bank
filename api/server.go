cd79297d8fca292e59dd015faa43b1ef30a323b2
joelpatel <joelpatel.cs@gmail.com>
2022-11-12 21:51:57 -0500

    add: server + API router + setup API routes

---
+++ api/server.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/joelpatel/go-bank/db/sqlc"
+)
+
+// serves all HTTP requests for the banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// creates a new Server instance and setups up routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{
+		store: store,
+	}
+
+	router := gin.Default()
+
+	// add routes to the router
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.getAccounts)
+
+	server.router = router
+	return server
+}
+
+// start an HTTP server on input address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
