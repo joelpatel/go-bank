b64f76d41d4a26766aaa4d7df16d6ea29e410a1d
joelpatel <joelpatel.cs@gmail.com>
2023-12-03 13:43:42 -0500

    setup database connection

---
+++ db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+var DBConn *sqlx.DB
+
+func InitializeDBConnection() {
+	host := os.Getenv("DATABASE_HOST")
+	port := os.Getenv("DATABASE_PORT")
+	user := os.Getenv("DATABASE_USER")
+	pass := os.Getenv("DATABASE_PASS")
+	dbName := os.Getenv("DATABASE_NAME")
+	sslMode := os.Getenv("DATABASE_SSLMODE")
+
+	databaseUrl := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, pass, dbName, sslMode)
+
+	db, err := sqlx.Open("pgx", databaseUrl)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	DBConn = db
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/joelpatel/go-bank/db"
+
+func main() {
+	db.InitializeDBConnection()
+}
