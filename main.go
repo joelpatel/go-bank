cadc48fd81953fedce27265824e3eb237da27ad5
joelpatel <joelpatel.cs@gmail.com>
2023-12-03 19:46:53 -0500

    code cleanup

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/joelpatel/go-bank/db"
-	"github.com/joelpatel/go-bank/db/controllers"
 )
 
 func main() {
 	db.InitializeDBConnection()
-
-	affected, err := controllers.DeleteAccountByID("1")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(affected)
 }
