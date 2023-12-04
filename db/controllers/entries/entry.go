3fc0676aa0677db4cb9c685b54b1d71432f02cb5
joelpatel <joelpatel.cs@gmail.com>
2023-12-04 18:18:05 -0500

    comment about amount is stored in cents

--- db/controllers/entries/entry.go
+++ db/controllers/entries/entry.go
@@ -5,6 +5,6 @@ import "time"
 type Entry struct {
 	ID        int64     `json:"id" db:"id"`
 	AccountID int64     `json:"account_id" db:"account_id"`
-	Amount    int64     `json:"amount" db:"amount"`
+	Amount    int64     `json:"amount" db:"amount"` // amount in cents
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
--- db/controllers/transfers/transfer.go
+++ db/controllers/transfers/transfer.go
@@ -6,6 +6,6 @@ type Transfer struct {
 	ID            int64     `json:"id" db:"id"`
 	FromAccountID int64     `json:"from_account_id" db:"from_account_id"`
 	ToAccountID   int64     `json:"to_account_id" db:"to_account_id"`
-	Amount        int64     `json:"amount" db:"amount"`
+	Amount        int64     `json:"amount" db:"amount"` // amount in cents
 	CreatedAt     time.Time `json:"created_at" db:"created_at"`
 }
