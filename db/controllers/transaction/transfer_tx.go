bd55bef20f0d7276acd5884f34782509f74f361b
joelpatel <joelpatel.cs@gmail.com>
2023-12-06 18:33:19 -0500

    code changes to avoid database deadlock by ordering account balance update operations in ascending order

--- db/controllers/transaction/transfer_tx.go
+++ db/controllers/transaction/transfer_tx.go
@@ -44,13 +44,13 @@ func TransferMoney(ctx context.Context, from_account_id, to_account_id, amount i
 		return nil, err
 	}
 
-	fromAccount, err := accounts.AddAccountBalance(ctx, tx, from_account_id, -amount)
-	if err != nil {
-		tx.Rollback()
-		return nil, err
+	var fromAccount, toAccount *accounts.Account
+	if from_account_id < to_account_id {
+		fromAccount, toAccount, err = addAmountInOrder(ctx, tx, from_account_id, to_account_id, -amount)
+	} else {
+		fromAccount, toAccount, err = addAmountInOrder(ctx, tx, to_account_id, from_account_id, amount)
 	}
 
-	toAccount, err := accounts.AddAccountBalance(ctx, tx, to_account_id, amount)
 	if err != nil {
 		tx.Rollback()
 		return nil, err
@@ -69,4 +69,16 @@ func TransferMoney(ctx context.Context, from_account_id, to_account_id, amount i
 		FromAccount:     *fromAccount,
 		ToAccount:       *toAccount,
 	}, nil
+}
+
+// add +amount to first_account
+// add -amount to second_account
+func addAmountInOrder(ctx context.Context, executor db.Executor, first_account_id, second_account_id, amount int64) (account1, account2 *accounts.Account, err error) {
+	account1, err = accounts.AddAccountBalance(ctx, executor, first_account_id, amount)
+	if err != nil {
+		return
+	}
+
+	account2, err = accounts.AddAccountBalance(ctx, executor, second_account_id, -amount)
+	return
 }
