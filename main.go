c15fb7447e00d995421550d0a86cdc7f58631964
joelpatel <joelpatel.cs@gmail.com>
2023-12-08 22:46:25 -0500

    moved loading of environment variables to main

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/joelpatel/go-bank/db"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
 	db.InitializeDBConnection()
 }
