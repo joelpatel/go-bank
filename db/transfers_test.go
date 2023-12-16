e08f81ee1efe53f1eb07474773636b5160327444
joelpatel <joelpatel.cs@gmail.com>
2023-12-16 15:57:08 -0500

    added unit tests for transfers table

--- db/transfers_test.go
+++ db/transfers_test.go
@@ -43,5 +43,117 @@ func TestGetTransferByID(t *testing.T) {
 }
 
 func TestGetTransfersFrom(t *testing.T) {
+	fromAccount := createRandomAccount(t)
+	expectedTransfers := make([]Transfer, 10)
 
+	for i := 0; i < 20; i++ {
+		if i%2 == 0 {
+			expectedTransfers[i/2] = *createRandomTransfer(t, fromAccount, createRandomAccount(t))
+		} else {
+			createRandomTransfer(t, createRandomAccount(t), createRandomAccount(t))
+		}
+	}
+
+	transfers, err := GetTransfersFromTo(context.Background(), testConn, fromAccount.ID, -1, 5, 0)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfers)
+
+	for i, transfer := range *transfers {
+		require.Equal(t, expectedTransfers[i].ID, transfer.ID)
+		require.Equal(t, expectedTransfers[i].FromAccountID, transfer.FromAccountID)
+		require.Equal(t, expectedTransfers[i].ToAccountID, transfer.ToAccountID)
+		require.Equal(t, expectedTransfers[i].Amount, transfer.Amount)
+		require.Equal(t, expectedTransfers[i].CreatedAt, transfer.CreatedAt)
+	}
+
+	transfers, err = GetTransfersFromTo(context.Background(), testConn, fromAccount.ID, -1, 5, 5)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfers)
+
+	for i, transfer := range *transfers {
+		require.Equal(t, expectedTransfers[i+5].ID, transfer.ID)
+		require.Equal(t, expectedTransfers[i+5].FromAccountID, transfer.FromAccountID)
+		require.Equal(t, expectedTransfers[i+5].ToAccountID, transfer.ToAccountID)
+		require.Equal(t, expectedTransfers[i+5].Amount, transfer.Amount)
+		require.Equal(t, expectedTransfers[i+5].CreatedAt, transfer.CreatedAt)
+	}
+}
+
+func TestGetTransfersTo(t *testing.T) {
+	toAccount := createRandomAccount(t)
+	expectedTransfers := make([]Transfer, 10)
+
+	for i := 0; i < 20; i++ {
+		if i%2 == 0 {
+			expectedTransfers[i/2] = *createRandomTransfer(t, createRandomAccount(t), toAccount)
+		} else {
+			createRandomTransfer(t, createRandomAccount(t), createRandomAccount(t))
+		}
+	}
+
+	transfers, err := GetTransfersFromTo(context.Background(), testConn, -1, toAccount.ID, 5, 0)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfers)
+
+	for i, transfer := range *transfers {
+		require.Equal(t, expectedTransfers[i].ID, transfer.ID)
+		require.Equal(t, expectedTransfers[i].FromAccountID, transfer.FromAccountID)
+		require.Equal(t, expectedTransfers[i].ToAccountID, transfer.ToAccountID)
+		require.Equal(t, expectedTransfers[i].Amount, transfer.Amount)
+		require.Equal(t, expectedTransfers[i].CreatedAt, transfer.CreatedAt)
+	}
+
+	transfers, err = GetTransfersFromTo(context.Background(), testConn, -1, toAccount.ID, 5, 5)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfers)
+
+	for i, transfer := range *transfers {
+		require.Equal(t, expectedTransfers[i+5].ID, transfer.ID)
+		require.Equal(t, expectedTransfers[i+5].FromAccountID, transfer.FromAccountID)
+		require.Equal(t, expectedTransfers[i+5].ToAccountID, transfer.ToAccountID)
+		require.Equal(t, expectedTransfers[i+5].Amount, transfer.Amount)
+		require.Equal(t, expectedTransfers[i+5].CreatedAt, transfer.CreatedAt)
+	}
+}
+
+func TestGetTransfersFromTo(t *testing.T) {
+	fromAccount := createRandomAccount(t)
+	toAccount := createRandomAccount(t)
+	var expectedTransfers []Transfer
+
+	for i := 0; i < 20; i++ {
+		if i%2 == 0 {
+			expectedTransfers = append(expectedTransfers, *createRandomTransfer(t, fromAccount, toAccount))
+		} else if i%3 == 0 {
+			expectedTransfers = append(expectedTransfers, *createRandomTransfer(t, fromAccount, createRandomAccount(t)))
+		} else if i%5 == 0 {
+			expectedTransfers = append(expectedTransfers, *createRandomTransfer(t, createRandomAccount(t), toAccount))
+		} else {
+			createRandomTransfer(t, createRandomAccount(t), createRandomAccount(t))
+		}
+	}
+
+	transfers, err := GetTransfersFromTo(context.Background(), testConn, fromAccount.ID, toAccount.ID, 5, 0)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfers)
+
+	for i, transfer := range *transfers {
+		require.Equal(t, expectedTransfers[i].ID, transfer.ID)
+		require.Equal(t, expectedTransfers[i].FromAccountID, transfer.FromAccountID)
+		require.Equal(t, expectedTransfers[i].ToAccountID, transfer.ToAccountID)
+		require.Equal(t, expectedTransfers[i].Amount, transfer.Amount)
+		require.Equal(t, expectedTransfers[i].CreatedAt, transfer.CreatedAt)
+	}
+
+	transfers, err = GetTransfersFromTo(context.Background(), testConn, fromAccount.ID, toAccount.ID, int64(len(expectedTransfers)-5), 5)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfers)
+
+	for i, transfer := range *transfers {
+		require.Equal(t, expectedTransfers[i+5].ID, transfer.ID)
+		require.Equal(t, expectedTransfers[i+5].FromAccountID, transfer.FromAccountID)
+		require.Equal(t, expectedTransfers[i+5].ToAccountID, transfer.ToAccountID)
+		require.Equal(t, expectedTransfers[i+5].Amount, transfer.Amount)
+		require.Equal(t, expectedTransfers[i+5].CreatedAt, transfer.CreatedAt)
+	}
 }
